63ce9dc971bd722d4475230bba87eaed8585e103
Kamran <skamranmanzoor@yahoo.com>
2019-07-11 00:28:28 +0100

    Added Vehicle Model

---
+++ Vega/Core/Models/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Core.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+
+        public ICollection<VehicleFeature> Features { get; set; }
+
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
+
+
+    }
+}
--- Vega/Persistence/VegaDbContext.cs
+++ Vega/Persistence/VegaDbContext.cs
@@ -9,15 +9,23 @@ namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
-        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
-        {
-        }
-
         public DbSet<Make> Makes { get; set; }
 
         public DbSet<Model> Models { get; set; }
 
         public DbSet<Feature> Features { get; set; }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+            new { vf.VehicleId, vf.FeatureId });
+        }
 
     }
 }
